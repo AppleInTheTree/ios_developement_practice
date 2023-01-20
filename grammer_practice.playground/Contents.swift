2ea98203a9dc55e7279ce3f858a9c741e316e076
AppleInTheTree <ejahn1175@gmail.com>
2023-01-20 16:45:38 +0700

    iOS practice

---
+++ grammer_practice.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//String Interpolation을 할수 있다 (\ 다음에 () 안에 코드 작성가능)
+print("The result of 1 + 2 + 3 + 4 = \(1+2+3+4)")
+
+
+//creating variable
+
+var number_a = 12
+var number_b = 5
+var temp = 0
+
+temp = number_a
+number_a = number_b
+number_b = temp
+
+print("a : \(number_a)")
+print("b : \(number_b)")
+
+
